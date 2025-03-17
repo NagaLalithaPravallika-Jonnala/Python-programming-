344e85fe3bc9d8fd3748b0aa076e7944ea866ded
NagaLalithaPravallika-Jonnala <sagarjonnala84@gmail.com>
2025-03-17 22:16:18 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,18 @@
+#calculator arithmetic oprators
+a=int(input())
+operator=input()
+b=int(input())
+if operator=='+':
+  print(a+b)
+elif operator=='-':
+  print(a-b)
+elif operator=='*':
+  print(a*b)
+elif operator=='/':
+  print(a/b)
+elif operator=='**':
+  print(a**b)
+elif operator=='//':
+  print(a//b)
+elif operator=='%':
+  print(a%b)
